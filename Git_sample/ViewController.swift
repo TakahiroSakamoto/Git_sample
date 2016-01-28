6f6d607326b636425366dc713a5a11a639e51a05
坂本貴宏 <takahirosakamoto@sakamototakahiro-no-MacBook-Pro.local>
2016-01-28 14:33:22 +0900

    ラベル作成

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -12,24 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
-        textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        textLabel.backgroundColor = UIColor.redColor()
-        textLabel.text = "TECH::CAMP"
-        textLabel.font = UIFont.systemFontOfSize(40)
-        textLabel.textColor = UIColor.whiteColor()
-        textLabel.shadowColor = UIColor.blueColor()
-        textLabel.textAlignment = NSTextAlignment.Center
-        textLabel.layer.masksToBounds = true
-        textLabel.layer.cornerRadius = 20.0
-        self.view.addSubview(textLabel)
+        let textLable = makeTextLable()
+        self.view.addSubview(textLable)
+     
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func makeTextLable() -> UILable {
+        let textLable = UILable(frame: CGRectMake(0, 0, 300, 100))
+        textLable.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+        textLable.backgroundColor = UIColor.redColor()
+        textLable.text = "TECH::CAMP"
+        textLable.font = UIFont.systemFontOfSize(40)
+        textLable.textColor = UIColor.whiteColor()
+        textLable.shadowColor = UIColor.blueColor()
+        textLable.textAlignment = NSTextAlignment.center
+        textLable.layer.masksToBounds = true
+        textLable.layer.cornerRadius = 20.0
+        return textLable
     }
 
 
